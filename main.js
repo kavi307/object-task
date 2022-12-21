444d5e15787f34489f7ee0e8b9d06518e7afec5c
kavi307 <ajkavitha7@gmail.com>
2022-12-21 10:57:02 +0530

    object

---
+++ main.js
@@ -0,0 +1,34 @@
+let student={
+    name:"kavitha",
+    age:20,
+    genter:"female",
+    degree:"computer science",
+    year:2022
+}
+    console.log(student);
+
+
+let places={
+    name:"surandai",
+    country:"india",
+    state:"tamilnadu",
+    distric:32
+}
+    console.log(places.country);
+
+let foot={
+    fav1:"chickenrice",
+    fav2:"muttonbiriyani",
+    fav3:"chicken",
+    fav4:"biriyani"
+}
+    console.log(foot);
+
+let month={
+    first:"jan",
+    second:"feb",
+    third:"march",
+    fourth:"apr"
+}
+    console.log(month);
+
